857caf4485611bd8675d8e4e6371e499ef3edeea
Matt McCarthy <mattrmc@gmail.com>
2018-11-17 17:38:37 -0600

    fix bg color

---
+++ src/components/home/Hexagons.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Hexagons extends Component {
+	render(){
+		return(
+			<h1>Do it</h1>
+		)
+	}
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,8 +3,11 @@ import { withSiteData } from 'react-static'
 //
 import logoImg from '../logo.png'
 
+import Hexagons from '../components/home/Hexagons';
+
 export default withSiteData(() => (
   <div>
     <h1 className="text-center">Cora Mylene</h1>
+    <Hexagons />
   </div>
 ))
